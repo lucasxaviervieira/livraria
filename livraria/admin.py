f7bca771a47d0d49e136d6dcd9f0985f1b2d54ec
lucasxaviervieira <lucasdavid.xavier@gmail.com>
2024-05-20 17:50:15 -0300

    Tutorial Step: 10

--- livraria/admin.py
+++ livraria/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Categoria, Editora
+from .models import Autor, Categoria, Editora, Livro
 
+admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Livro)
